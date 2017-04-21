d2dea06eae92e4c6577d3403c1253b369de4684c
LokoSama <jeremy.basso.31@gmail.com>
2017-04-21 17:02:39 +0200

    Avancée dataManager

--- darwin/dataManager.ml
+++ darwin/dataManager.ml
@@ -34,6 +34,10 @@ privées :
 - 
 
 *)
+
+open Types
+
+(** TAILLE MAP **)
 let map_width = 45 ;; (* TODO à récupérer par requête *)
 
 let map_height = 45 ;; (* TODO à récupérer par requête *)
@@ -42,77 +46,119 @@ let our_jid = 1 ;; (* TODO à récupérer par requête *)
 
 let current_turn = 0 ;; (* TODO à récupérer par requête *)
 
+(***** CARTES *****)
+
 (* Carte du terrain (terrain ou ville ou autre) *)
-let type terrain = Ground | Water | Ally | Ennemy | Neutral | Unknown  ;;
+type terrain = Ground | Water | Ally | Ennemy | Neutral | Unknown  ;;
 
 let map_terrain = Array.make_matrix map_width map_height Unknown ;;
 
 let fill_terrain terrain q r =
-match terrain with 
-| "water" -> map_terrain.(q).(r) <- Water
-| "ground"-> map_terrain.(q).(r) <- Ground
-| "our_city" -> map_terrain.(q).(r) <- Ally
-| "their_city" -> map_terrain.(q).(r) <- Ennemy
-| "city" -> map_terrain.(q).(r) <- Neutral
+  match terrain with 
+    | "water" -> map_terrain.(q).(r) <- Water
+    | "ground"-> map_terrain.(q).(r) <- Ground
+    | "our_city" -> map_terrain.(q).(r) <- Ally
+    | "their_city" -> map_terrain.(q).(r) <- Ennemy
+    | "city" -> map_terrain.(q).(r) <- Neutral
+    | _ -> failwith "erreur fill_terrain"
+
 ;;
 
-(* Carte ennemis *)
-let type ennemi = City | Unit | Other ;;
+
+(** Non pertinent ? **)
+
+(* Carte ennemis 
+type ennemi = City | Unit | Other ;;
 
 let map_ennemi = Array.make_matrix map_width map_height Other ;;
+*)
 
-(*Liste unités *)
+(***** LISTES *****)
 
+(** Listes unités **)
 
+(* Unités alliées *)
+type unite_list = {q:int; r:int; pid : int ; unite_type : unites ; hp : int};;
+
+let liste_unites = [];;
+
+let update_unite_alliee q r pid unite_type hp = 
+  	let pid_is_not pid element = element.pid != pid in
+    	{q=q; r=r ; pid=pid; unite_type=unite_type; hp=hp} :: (List.filter (pid_is_not pid) liste_unites) ;;
+
+(* Unités ennemies *)
+
+(* TODO : liste d'unites (pos, pid, type unité, hp? (pour battleship)) *)
+
+(** Listes villes **)
 (*Liste villes alliées*)
 
-type unite = Army | Fight | Battleship | Patrol | Transport | None ;;
+type (*ville*) allie = {q : int ;r : int ;cid : int ; prod : unites ; tours_restants : int} ;;
 
-type allie = {q : int ;r : int ;cid : int ; prod : unite ; tours_restants : int} ;;
-
-let liste_allie = [] ;;
+let liste_ville_alliee = [] ;;
 
 (* a utiliser avec listealliee = add_alliee *)
-let add_ville_allie q r cid = 
-	let cid_is cid element = element.cid == cid in
-	if List.exists (cid_is cid) liste_allie then liste_allie
-	else liste_allie :: { q =q ; r =r ; cid = cid ; prod = None ; tours_restants = -1}
- ;;
+let update_ville_allie q r cid = 
+    	if List.exists (cid_is cid) liste_ville_alliee then liste_ville_alliee
+    	else {q =q ; r =r ; cid = cid ; prod = None ; tours_restants = -1} :: liste_ville_alliee ;;
 
 (* a utiliser avec listealliee = rm_alliee *)
 let rec rm_allie rmcid =
-let pred id alpha = alpha.cid != id in
-List.filter (pred rmcid) liste_allie
+  let pred id alpha = alpha.cid != id in
+    List.filter (pred rmcid) liste_ville_alliee
 ;;  
 
 (* Liste villes ennemies *)
+let liste_ville_ennemie = [] ;;
 
-(* TODO 
-	add, remove, structure de données *)
+type (*ville*) ennemi = {q : int ;r : int ;cid : int } ;;
 
+let add_ville_ennemi q r cid = 
+  	let cid_is cid element = element.cid == cid in
+    	if List.exists (cid_is cid) liste_ville_ennemie then liste_ville_ennemie
+    	else  { q =q ; r =r ; cid = cid } :: liste_ville_ennemie ;;
+
+let rec rm_ennemi rmcid =
+  let pred id alpha = alpha.cid != id in
+    List.filter (pred rmcid) liste_ville_ennemie
+;;  
+
+(* Liste ville neutres *)
+
+(***** OUTILS *****)
+
+(* permet de calculer la vraie distance  entre deux cases *)
+let tiles_distance (qa, ra) (qb, rb) = (abs (qa - qb) + abs (qa + ra - qb - rb) + abs (ra - rb)) / 2
+
+(* TODO ajouter d'autres *)
+
+(***** TRAITEMENT *****)
 (*Traitement des informations*)
 let traiter_set_visible args =
-	match args with
-	| [ q ; r ; terrain ; "none" ] -> fill_terrain terrain map_terrain q r
+  	match args with
+    	| [ q ; r ; terrain ; "none" ] -> fill_terrain terrain map_terrain q r
     | [ q ; r ; terrain ; "city" ; cid ] -> fill_terrain "city" q r 
-    | [ q ; r ; terrain ; "owned_city" ; cid ; jid ] -> if jid = our_jid then (fill_terrain "our_city" q r ; add_ville_allie q r cid) else (fill_terrain "their_city" q r ; add_ville_ennemy q r cid)
-    | [ q ; r ; terrain ; "piece" ; jid ; pid ; ptid ; hits ] -> 
+    | [ q ; r ; terrain ; "owned_city" ; cid ; jid ] -> if jid = our_jid then (fill_terrain "our_city" q r ; add_ville_allie q r cid) else (fill_terrain "their_city" q r ; add_ville_ennemi q r cid)
+    | [ q ; r ; terrain ; "piece" ; jid ; pid ; ptid ; hits ] -> if jid = our_jid then (update_unite_alliee q r pid ptid hp) else (update_unite_ennemie q r pid ptid hp)
+		| _ -> failwith "erreur traiter_set_visible"
+		
+let traiter_set_explored args =
+match
+        (* TODO : 
+           | "set_explored" -> traiter_set_explored tlMsg
+           |  "delete_piece" -> traiter_delete_piece tlMsg
+           |  "create_piece" -> traiter_create_piece tlMsg
+           |  "move" -> traiter_move tlMsg
+           |  "lose_city" -> traiter_lose_city tlMsg
+           |  "leave_terrain" -> traiter_leave_terrain tlMsg
+           |  "enter_city" -> traiter_enter_city tlMsg
+           |  "enter_piece" -> traiter_enter_piece tlMsg
+           |  "leave_city" -> traiter_leave_city tlMsg
+           |  "leave_piece" -> traiter_leave_piece tlMsg
+           |  "ok-invasion" -> traiter_ok-invasion tlMsg
+           |  "ko-invasion" -> traiter_ko-invasion tlMsg
+           |  "city-units-limit" -> traiter_city-units-limit tlMsg
+           |  "created-units-limit" -> traiter_created-units-limit tlMsg
 
-(* TODO : 
-    | "set_explored" -> traiter_set_explored tlMsg
-    |  "delete_piece" -> traiter_delete_piece tlMsg
-    |  "create_piece" -> traiter_create_piece tlMsg
-    |  "move" -> traiter_move tlMsg
-    |  "lose_city" -> traiter_lose_city tlMsg
-    |  "leave_terrain" -> traiter_leave_terrain tlMsg
-    |  "enter_city" -> traiter_enter_city tlMsg
-    |  "enter_piece" -> traiter_enter_piece tlMsg
-    |  "leave_city" -> traiter_leave_city tlMsg
-    |  "leave_piece" -> traiter_leave_piece tlMsg
-    |  "ok-invasion" -> traiter_ok-invasion tlMsg
-    |  "ko-invasion" -> traiter_ko-invasion tlMsg
-    |  "city-units-limit" -> traiter_city-units-limit tlMsg
-    |  "created-units-limit" -> traiter_created-units-limit tlMsg
 
-
-*)
+        *)
