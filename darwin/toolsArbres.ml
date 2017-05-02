b27a3ded3e85f096a356f3bf037dece42dbce7cf
LokoSama <jeremy.basso.31@gmail.com>
2017-05-02 10:46:28 +0200

    avancée croisement, ajout outils arbres

---
+++ darwin/toolsArbres.ml
@@ -0,0 +1,12 @@
+ (* fonction qui retourne la profondeur d'un arbre *)
+ let rec depth = function
+| Leaf _ -> 0
+| Node (a,b,c) -> 1 + max (depth a) (depth c)
+
+(* fonction qui remplace quand le prédicat est vrai*)
+ let rec replace pred sub tree =
+  if pred tree then sub
+  else match tree with
+       | Leaf _ -> tree
+       | Node (a,b,c) -> Node (replace pred sub a, b,replace pred sub c)
+;;
