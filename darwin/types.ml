1b352684ba20b59d3b768437196a6d4ac0f9c998
JohnAnthony795 <johnanthony795@laposte.net>
2017-05-11 09:44:57 +0200

    Quelques changements pour remove les warning

--- darwin/types.ml
+++ darwin/types.ml
@@ -33,20 +33,18 @@ type t_action =
   | Move of move
   | Set_city_prod of set_city_production
   | End_turn
-;;
+  
 (* TODO : rajouter l'action pass pour qu'une unité ne joue pas immédiatement *)
 
 type comparateur = Inf | Sup | Eq | InfEq | SupEq;;
 
 type t_predicat= (* rajouter des prédicats en masse*)
-    Nb_unite_allie_proche of (unites * int * comparateur)
+  | Nb_unite_allie_proche of (unites * int * comparateur)
   |Nb_ville_allie_proche of (int * comparateur)
-;;
 
 type t_arbre =
-    Leaf of t_action
+  | Leaf of t_action
   |Node of (t_arbre * t_predicat * t_arbre)
-;;
 
 (*type arbre_de =
   	 Army_tree of t_arbre
@@ -68,75 +66,61 @@ let arbre0 = Node (Leaf End_turn,Nb_unite_allie_proche (ARMY,7,Inf),Node (Leaf E
 (*fonctions de print verbeux des types:*)
 
 let comparateur_to_string c = match c with
-    Inf -> "<"
+  | Inf -> "<"
   | Sup -> ">"
   | Eq -> "="
   | InfEq -> "<="
   | SupEq -> ">="
-;;
 
 let unite_to_string u = match u with
-    ARMY -> "ARMY"
+  | ARMY -> "ARMY"
   | TRANSPORT -> "TRANSPORT"
   | FIGHT -> "FIGHT"
   | BATTLESHIP -> "BATTLESHIP"
   | PATROL -> "PATROL"
-;;
 
 let direction_to_string d = match d with
-    Up -> "UP"
+  | Up -> "UP"
   | Down -> "DOWN"
   | Right -> "RIGHT"
   | Left -> "LEFT"
   | Upleft -> "UPLEFT"
   | Downright -> "DOWNRIGHT" (*darius*)
-;;
-
 
 (*fonctions publiques de print/string*)
 let pred_to_string p = match p with   (*tenir à jour avec les prédicats ;)*)
-    Nb_unite_allie_proche (u,n,c) -> "Nb de "^ (unite_to_string u) ^ " allies proche " ^ (comparateur_to_string c) ^ " " ^ (string_of_int n) ^ "?"
-  |Nb_ville_allie_proche (n,c) -> "Nb de villes allies proche " ^ (comparateur_to_string c) ^ " " ^ (string_of_int n) ^ "?"
-  |_ -> "predicat"
-;;
-let action_to_string a = match a with
-    Move (id,dir) -> "Move id°" ^ (string_of_int id) ^ " " ^ (direction_to_string dir)
-  |Set_city_prod (id,p_type) -> "Set_city_prod id°" ^ (string_of_int id) ^ " " ^ (unite_to_string p_type) (*clarifier le piece type id avec un piece type id to string??*)
-  |End_turn -> "End_turn"
-  |_ -> "action"
-;;
+  | Nb_unite_allie_proche (u,n,c) -> "Nb de "^ (unite_to_string u) ^ " allies proche " ^ (comparateur_to_string c) ^ " " ^ (string_of_int n) ^ "?"
+  | Nb_ville_allie_proche (n,c) -> "Nb de villes allies proche " ^ (comparateur_to_string c) ^ " " ^ (string_of_int n) ^ "?"
 
+let action_to_string a = match a with
+  | Move (id,dir) -> "Move id°" ^ (string_of_int id) ^ " " ^ (direction_to_string dir)
+  | Set_city_prod (id,p_type) -> "Set_city_prod id°" ^ (string_of_int id) ^ " " ^ (unite_to_string p_type) (*clarifier le piece type id avec un piece type id to string??*)
+  | End_turn -> "End_turn"
 
 (*variantes de print moins verbeuses*)
 let unite_to_code u = match u with
-    ARMY -> "A"
+  | ARMY -> "A"
   | TRANSPORT -> "T"
   | FIGHT -> "F"
   | BATTLESHIP -> "B"
   | PATROL -> "P"
-;;
 
 let direction_to_code d = match d with
-    Up -> "U"
+  | Up -> "U"
   | Down -> "D"
   | Right -> "R"
   | Left -> "L"
   | Upleft -> "UL"
   | Downright -> "DR" 
-;;
 
 let pred_to_code p = match p with   (*tenir à jour avec les prédicats ;)  STANDARD : mettre un '?' au debut *)
-    Nb_unite_allie_proche (u,n,c) -> "?NBUAP:" ^ (unite_to_code u) ^ ":" ^ (string_of_int n) ^ ":" ^ (comparateur_to_string c) 
-  |Nb_ville_allie_proche (n,c) -> "?NBVAP:" ^ (string_of_int n) ^ ":" ^ (comparateur_to_string c) 
-  |_ -> "predicat"
-;;
+  | Nb_unite_allie_proche (u,n,c) -> "?NBUAP:" ^ (unite_to_code u) ^ ":" ^ (string_of_int n) ^ ":" ^ (comparateur_to_string c) 
+  | Nb_ville_allie_proche (n,c) -> "?NBVAP:" ^ (string_of_int n) ^ ":" ^ (comparateur_to_string c) 
 
 let action_to_code a = match a with (* tenir a jour aussi  STANDARD : mettre un '!' au debut *)
-    Move (id,dir) -> "!MV:" ^ (string_of_int id) ^ ":" ^ (direction_to_code dir)
-  |Set_city_prod (id,p_type) -> "!SCP:°" ^ (string_of_int id) ^ ":" ^ (unite_to_code p_type) (*clarifier le piece type id avec un piece type id to string??*)
-  |End_turn -> "!ET"
-  |_ -> "action"
-;;
+  | Move (id,dir) -> "!MV:" ^ (string_of_int id) ^ ":" ^ (direction_to_code dir)
+  | Set_city_prod (id,p_type) -> "!SCP:°" ^ (string_of_int id) ^ ":" ^ (unite_to_code p_type) (*clarifier le piece type id avec un piece type id to string??*)
+  | End_turn -> "!ET"
 
 (*fonctions principales*)
 let print_action a d = printf "%s\n" ((String.make d '~') ^ (String.make d '~') ^ (action_to_string a));; (*moche mais on peut mettre que un carac*)
