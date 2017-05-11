e1d598b04920e554d7daafd0a83d0995cb158438
LokoSama <jeremy.basso.31@gmail.com>
2017-05-11 10:17:55 +0200

    Maj croisement

--- darwin/genetique/croisement.ml
+++ darwin/genetique/croisement.ml
@@ -6,6 +6,15 @@ In both case we have 2 trees as output
 
  *)
  
+ 
+ let eval = 0.25
+ ;;
+ 
+ let ratio = Random.float 1.0
+ ;;
+ 
+ 
+ 
  let rec depth = function
 | Leaf _ -> 0
 | Node (a,b,c) -> 1 + max (depth a) (depth c)
@@ -14,9 +23,9 @@ In both case we have 2 trees as output
  let rec get_random_node arbre = 
  match arbre with
  | Node (Leaf l1,b,Leaf l2) -> Node (Leaf l1,b,Leaf l2) (* 2 feuilles donc on retourne ce node *)
- | Node (Leaf l,b,c) -> if Random.bool() then Node(Leaf l,b,c) else get_random_node c (*1/2 on retourne cette node sinon on poursuit*)
- | Node (a,b,Leaf l) -> if Random.bool() then Node(a,b,Leaf l) else get_random_node a (*1/2 on retourne cette node sinon on poursuit*)
- | Node (a,b,c) -> if Random.bool() then Node (a,b,c) else (if Random.bool() then get_random_node a else get_random_node c) (*1/2 on retourne cette node sinon 1/2 sur chaque branche*)
+ | Node (Leaf l,b,c) -> if (ratio < eval) then Node(Leaf l,b,c) else get_random_node c (*1/2 on retourne cette node sinon on poursuit*)
+ | Node (a,b,Leaf l) -> if(ratio < eval) then Node(a,b,Leaf l) else get_random_node a (*1/2 on retourne cette node sinon on poursuit*)
+ | Node (a,b,c) -> if (ratio < eval) then Node (a,b,c) else (if (ratio < eval) then get_random_node a else get_random_node c) (*1/2 on retourne cette node sinon 1/2 sur chaque branche*)
  | Leaf _ -> failwith ("rip");
  ;;
  
