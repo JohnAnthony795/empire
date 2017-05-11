436df31f8f8ff2259fab7bffdf39f5ae072af9d4
JohnAnthony795 <johnanthony795@laposte.net>
2017-05-11 10:04:40 +0200

    Correctifs

--- darwin/genetique/typesGen.ml
+++ darwin/genetique/typesGen.ml
@@ -1,12 +1,6 @@
 
-open ../Types
-
-type score : float (* Supposé entre 0 et 1 *)
-
-type t_candidat : t_foret * score
-
-type t_population : t_candidat list
-
-
-
+let print_population pop =
+	List.map (fun x -> match x with 
+		| (foret,score) -> match foret with
+			| (a,b,c,d,e,f) -> print_tree a 0;Printf.printf "\n%!";print_tree b 0;Printf.printf "\n%!";print_tree c 0;Printf.printf "\n%!";print_tree d 0;Printf.printf "\n%!";print_tree e 0;Printf.printf "\n%!";print_tree f 0) pop;;
 
